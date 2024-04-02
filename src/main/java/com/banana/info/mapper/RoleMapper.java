eb2f30f4bab8b0b53ec6c450bf5372348fe92d95
Banana-HAZS <2962125830@qq.com>
2024-04-02 19:04:13 +0800

    4/2

---
+++ src/main/java/com/banana/info/mapper/RoleMapper.java
@@ -0,0 +1,20 @@
+package com.banana.info.mapper;
+
+import com.banana.info.entity.param.RoleParam;
+import com.banana.info.entity.vo.RoleVO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zjy
+ * @since 2023-12-19
+ */
+public interface RoleMapper extends BaseMapper<RoleVO> {
+
+    Page<RoleVO> getRolePage(@Param("param") RoleParam param, Page<RoleVO> page);
+}
