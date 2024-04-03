ba21824b3cac468eb3730b5862a47a689ab0baf7
Banana-HAZS <2962125830@qq.com>
2024-04-03 17:17:38 +0800

    4/3

--- src/main/java/com/banana/info/controller/LoanController.java
+++ src/main/java/com/banana/info/controller/LoanController.java
@@ -1,8 +1,18 @@
 package com.banana.info.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.banana.common.Result;
+import com.banana.info.entity.Loan;
+import com.banana.info.service.ILoanService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,4 +26,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/info/loan")
 public class LoanController {
 
+    @Autowired
+    private ILoanService loanService;
+
+    @GetMapping("all")
+    public Result<List<Loan>> getAllLoan(){
+        return null;
+    }
+
+    @GetMapping("/list")
+    public Result<Map<String,Object>> getLoanList(){
+
+        return null;
+    }
+
+    @PostMapping
+    public Result<?> addLoan(@RequestBody Loan loan){
+        return null;
+    }
+
+    @PutMapping
+    public Result<?> updateLoan(@RequestBody Loan loan){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Result<Loan> getLoanById(@PathVariable("id") Integer id){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public Result<Loan> deleteById(@PathVariable("id") Integer id){
+        return null;
+    }
 }
---
+++ src/main/java/com/banana/info/entity/vo/LoanApplyVO.java
@@ -0,0 +1,164 @@
+package com.banana.info.entity.vo;
+
+import com.banana.info.entity.Employee;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zjy
+ * @since 2024-03-29
+ */
+@Data
+public class LoanApplyVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 唯一标识
+     */
+    private Integer id;
+
+    /**
+     * 客户id
+     */
+    private Integer customerId;
+
+    /**
+     * 客户账号
+     */
+    private String customerAccount;
+
+    /**
+     * 客户姓名
+     */
+    private String customerName;
+
+    /**
+     * 贷款金额
+     */
+    private BigDecimal price;
+
+    /**
+     * 贷款类型(1个人贷款、2商业贷款)
+     */
+    private Integer loanType;
+
+    /**
+     * 申请日期
+     */
+    private LocalDateTime applyDate;
+
+    /**
+     * 审核状态(1待审核、2审核中、3已通过、4已驳回)
+     */
+    private Integer auditType;
+
+    /**
+     * 审核人id
+     */
+    private Integer auditorId;
+
+    /**
+     * 审核人
+     */
+    private String auditorName;
+
+    /**
+     * 审核日期
+     */
+    private LocalDateTime auditDate;
+
+    /**
+     * 审核意见
+     */
+    private String auditOpinion;
+
+    /**
+     * 贷款发放执行人id
+     */
+    private Integer grantExecutorId;
+
+    /**
+     * 贷款发放执行人
+     */
+    private Integer grantExecutor;
+
+    /**
+     * 贷款发放日期
+     */
+    private LocalDateTime grantDate;
+
+    /**
+     * 贷款状态(1等待审核、2待放款、3放款中、4已放款、5已驳回、6已结清)
+     */
+    private Integer loanStatus;
+
+    /**
+     * 利率
+     */
+    private BigDecimal interestRate;
+
+    /**
+     * 还款日期(每一期的还款日期)
+     */
+    private LocalDateTime repayDate;
+
+    /**
+     * 每期还款本金
+     */
+    private BigDecimal termRepayPrincipal;
+
+    /**
+     * 每期还款利息
+     */
+    private BigDecimal termRepayInterest;
+
+    /**
+     * 每期还款金额(本金+利息)
+     */
+    private BigDecimal termRepayPrice;
+
+    /**
+     * 还款方式(1等额本息还款、2等额本金还款、3按期付息、4分期还款)
+     */
+    private Integer repayMethod;
+
+    /**
+     * 还款期数(月)
+     */
+    private Integer repayTerm;
+
+    /**
+     * 贷款期限(最后的结清日期)
+     */
+    private LocalDateTime loanDate;
+
+    /**
+     * 逾期罚息基数(130%-150%)
+     */
+    private BigDecimal lateChargeBase;
+
+    /**
+     * 贷款用途
+     */
+    private String loanPurpose;
+
+    /**
+     * 已收回利息
+     */
+    private BigDecimal recoveredInterest;
+
+    /**
+     * 客户联系方式
+     */
+    private String customerPhone;
+}
